0dcbafb9e16e3832fb3c837d9bb1150a14fb47fb
Icaro Tavares <icarotavareslima@outlook.com>
2023-09-14 17:48:59 -0400

    fix: request entity larger error

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const port = process.env.API_PORT || 9324;
+
+  const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '300mb' }));
+  app.use(urlencoded({ limit: '300mb', extended: true }));
 
   await app.listen(port).then(() => {
     console.log(`OpenDentalSyncAPI started on port ${port}`);
